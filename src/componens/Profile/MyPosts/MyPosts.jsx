7d235c70a247341a92cbd08e14738bf317102203
Vladimir Voloshchik <lovkiy2012@gmail.com>
2019-09-22 00:03:37 +0300

    Revert "add MyPosts"
    This reverts commit 76146498522e8de8792f45fdf46fa76dd65b6b9e.

--- src/componens/Profile/MyPosts/MyPosts.jsx
+++ src/componens/Profile/MyPosts/MyPosts.jsx
@@ -6,10 +6,7 @@ const MyPosts = () => {
       <div>
         My post
         <div>new post</div>
-        <div className={s.item}>
-          <img src="https://klike.net/uploads/posts/2019-03/1551511804_3.jpg" alt=""/>
-          post1
-          <div><span>like</span></div></div>
+        <div className={s.item}>post1</div>
         <div className={s.item}> post2</div>
         <div className="item"> post3</div>
         <div className="item"> post4</div>
