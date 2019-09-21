380795e21641c7287bab13080d1f983f534d1833
Vladimir Voloshchik <lovkiy2012@gmail.com>
2019-09-22 00:52:28 +0300

    add avatar

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
         <div>
             <div>
                 <img
-                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+                    src="https://cdn.pixabay.com/photo/2018/06/07/04/48/shanghai-3459422__340.jpg"/>
             </div>
             <div className={s.descriptionBlock}>
                 ava + description
