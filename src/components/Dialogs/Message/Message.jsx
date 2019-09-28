05df9f4a20c28846c99d1443bd8d50601c775873
Vladimir Voloshchik <lovkiy2012@gmail.com>
2019-09-28 20:57:09 +0300

    message

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,9 +3,11 @@ import s from './../Dialogs.module.css';
 
 const Message = (props) => {
     return <div className={s.dialog}>{props.message}
+    
+    <div><textarea name="message" id="message" cols="20" rows="1"></textarea></div>
     <div><button>добавить</button>
-    <div><textarea name="message" id="message" cols="5" rows="5"></textarea></div>
     </div>
+
     
     </div>
 }
