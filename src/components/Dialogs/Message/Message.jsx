6023686e4b783d402ec36c9858eea87cb140498b
Vladimir Voloshchik <lovkiy2012@gmail.com>
2019-09-29 21:16:43 +0300

    message delet textarea

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,15 +1,8 @@
-import React from 'react';
-import s from './../Dialogs.module.css';
+import React from "react";
+import s from "./../Dialogs.module.css";
 
-const Message = (props) => {
-    return <div className={s.dialog}>{props.message}
-    
-    <div><textarea name="message" id="message" cols="20" rows="1"></textarea></div>
-    <div><button>добавить!</button>
-    </div>
-
-    
-    </div>
-}
+const Message = props => {
+    return <div className={s.dialog}>{props.message}</div>;
+};
 
 export default Message;
